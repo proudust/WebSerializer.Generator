6466059e4a4bcd6830513bb4293a88f752b45106
Proudust <proudust@gmail.com>
2024-08-20 22:35:19 +0900

    :sparkles: Create Project

---
+++ src/WebSerializer.Generator/WebSerializerGenerator.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+
+namespace Proudust.WebSerializer.Generator;
+
+[Generator(LanguageNames.CSharp)]
+public sealed partial class WebSerializerGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+
+    }
+}
